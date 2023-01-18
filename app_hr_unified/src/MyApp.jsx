ddc9262a7183e23284af018b199874b9880162b7
Kunal Dixit <contactkunaldixit@gmail.com>
2023-01-18 22:33:33 +0800

    Initial commit

---
+++ app_hr_unified/src/MyApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const MyApp = () => {
+	return (
+		<React.Fragment>
+			<CssBaseline />
+			Hello World
+		</React.Fragment>
+	);
+};
+
+export default MyApp;
